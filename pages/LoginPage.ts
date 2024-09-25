1b4db6c123f3b5024468432b6f47c90cc7c442cb
rijviahmedrahat <angelnishat4746@gmail.com>
2024-09-25 11:52:18 +0600

    a

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -6,24 +6,20 @@ export class LoginPage {
     constructor(page: Page) {
         this.page = page;
     }
-
     async navigateTo() {
         await this.page.goto('https://devtest.isec.gov.bd/');
     }
-
     async clickLoginLink() {
         await this.page.getByRole('link', { name: 'Login' }).click();
     }
-
     async enterUsername(username: string) {
-        await this.page.getByTestId('login-page-username-input').fill(username);
+        await this.page.getByTestId('login-page-username-input').fill(username)
     }
-
     async enterPassword(password: string) {
         await this.page.getByTestId('login-page-password-input').fill(password);
     }
 
     async clickLoginButton() {
-        await this.page.getByTestId('login-page-continue-login-button').click();
+        await this.page.getByTestId('login-page-continue-login-button').click()
     }
 }
