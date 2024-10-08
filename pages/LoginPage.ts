c3d389293464b6ce7def0210c854cce27413b08a
rijviahmedrahat <angelnishat4746@gmail.com>
2024-10-08 17:03:46 +0600

    ofc

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -16,10 +16,10 @@ export class LoginPage {
         await this.page.getByTestId('login-page-username-input').fill(username);
     }
     async enterPassword(password: string) {
-        await this.page.getByTestId('login-page-password-input').fill(password);
+        await this.page.getByTestId('login-page-password-input').fill(password)
     }
 
     async clickLoginButton() {
-        await this.page.getByTestId('login-page-continue-login-button').click();
+        await this.page.getByTestId('login-page-continue-login-button').click()
     }
 }
